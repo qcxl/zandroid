2ec0b37c6aabdce2d080c7d2da770e4f5d93a8ea
zouchongjin <zouchongjin@sina.com>
2015-07-08 09:19:27 +0800

    ...

--- src/com/zcj/android/util/UtilAndroid.java
+++ src/com/zcj/android/util/UtilAndroid.java
@@ -1,10 +1,13 @@
 package com.zcj.android.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -15,6 +18,7 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.provider.Settings.Secure;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.WindowManager;
@@ -219,34 +223,44 @@ public class UtilAndroid {
 	}
 
 	/**
-	 * 获取IMEI(国际移动装备辨识码)，返回NULL表示获取失败
+	 * 获取IMEI(国际移动装备辨识码)，返回NULL表示获取失败，如果是双卡，则两个值通过“&”字符拼接
 	 * <p>
 	 * 需要的权限： {@link android.Manifest.permission#READ_PHONE_STATE
 	 * android.permission.READ_PHONE_STATE}
 	 */
 	public static String getIMEI(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imei = telephonyManager.getDeviceId();
-		if (imei != null && imei.length() >= 10 && !"358673013795895".equals(imei) && !"004999010640000".equals(imei)
-				&& !"00000000000000".equals(imei) && !"000000000000000".equals(imei)) {
-			return imei;
+		DoubleCardInfo doubleInfo = doubleSimInfo(context);
+		if (doubleInfo == null) {
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			String imei = telephonyManager.getDeviceId();
+			if (imei != null && imei.length() >= 10 && !"358673013795895".equals(imei) && !"004999010640000".equals(imei)
+					&& !"00000000000000".equals(imei) && !"000000000000000".equals(imei)) {
+				return imei;
+			}
+			return null;
+		} else {
+			return doubleInfo.getImei_1() + "&" + doubleInfo.getImei_2();
 		}
-		return null;
 	}
 
 	/**
-	 * 获取IMSI(国际移动用户识别码)，返回NULL表示获取失败
+	 * 获取IMSI(国际移动用户识别码)，返回NULL表示获取失败，如果是双卡，则两个值通过“&”字符拼接
 	 * <p>
 	 * 需要的权限： {@link android.Manifest.permission#READ_PHONE_STATE
 	 * android.permission.READ_PHONE_STATE}
 	 */
 	public static String getIMSI(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imsi = telephonyManager.getSubscriberId();
-		if (imsi != null && imsi.length() > 0) {
-			return imsi;
+		DoubleCardInfo doubleInfo = doubleSimInfo(context);
+		if (doubleInfo == null) {
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			String imsi = telephonyManager.getSubscriberId();
+			if (imsi != null && imsi.length() > 0) {
+				return imsi;
+			}
+			return null;
+		} else {
+			return doubleInfo.getImsi_1() + "&" + doubleInfo.getImsi_2();
 		}
-		return null;
 	}
 
 	/** 获取mac地址，返回NULL表示获取失败 */
@@ -260,6 +274,176 @@ public class UtilAndroid {
 		} catch (Exception e) {
 		}
 		return null;
+	}
+	
+	/** 获取蓝牙MAC地址，返回NULL表示获取失败（蓝牙打开状态才能获取） */
+	public static String getBluetoothMacAddress() {
+		try {
+			BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			return bluetoothAdapter.getAddress();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/** 如果是双卡手机，则返回双卡手机信息(包括两个IMEI和两个IMSI )；如果不是，则返回NULL */
+	private static DoubleCardInfo doubleSimInfo(Context c) {
+		DoubleCardInfo gaotongDoubleInfo = initQualcommDoubleSim(c);
+		boolean isGaoTongCpu = gaotongDoubleInfo.isGaotongDoubleSim();
+		if (isGaoTongCpu) {
+			// 高通芯片双卡
+			return gaotongDoubleInfo;
+		}
+		DoubleCardInfo mtkDoubleInfo = initMtkDoubleSim(c);
+		boolean isMtkCpu = mtkDoubleInfo.isMtkDoubleSim();
+		if (isMtkCpu) {
+			// MTK芯片双卡
+			return mtkDoubleInfo;
+		}
+		// 普通单卡手机
+		return null;
+	}
+	
+	@SuppressWarnings("unused")
+	private static class DoubleCardInfo {
+		private Integer simId_1;
+		private Integer simId_2;
+		private String imsi_1;
+		private String imsi_2;
+		private String imei_1;
+		private String imei_2;
+		private String defaultImsi;
+		private Integer phoneType_1;
+		private Integer phoneType_2;
+		private boolean mtkDoubleSim;
+		private boolean gaotongDoubleSim;
+		public boolean isGaotongDoubleSim() {
+			return gaotongDoubleSim;
+		}
+		public void setGaotongDoubleSim(boolean gaotongDoubleSim) {
+			this.gaotongDoubleSim = gaotongDoubleSim;
+		}
+		public boolean isMtkDoubleSim() {
+			return mtkDoubleSim;
+		}
+		public void setMtkDoubleSim(boolean mtkDoubleSim) {
+			this.mtkDoubleSim = mtkDoubleSim;
+		}
+		public Integer getPhoneType_1() {
+			return phoneType_1;
+		}
+		public void setPhoneType_1(Integer phoneType_1) {
+			this.phoneType_1 = phoneType_1;
+		}
+		public Integer getPhoneType_2() {
+			return phoneType_2;
+		}
+		public void setPhoneType_2(Integer phoneType_2) {
+			this.phoneType_2 = phoneType_2;
+		}
+		public String getDefaultImsi() {
+			return defaultImsi;
+		}
+		public void setDefaultImsi(String defaultImsi) {
+			this.defaultImsi = defaultImsi;
+		}
+		public String getImsi_1() {
+			return imsi_1;
+		}
+		public void setImsi_1(String imsi_1) {
+			this.imsi_1 = imsi_1;
+		}
+		public String getImsi_2() {
+			return imsi_2;
+		}
+		public void setImsi_2(String imsi_2) {
+			this.imsi_2 = imsi_2;
+		}
+		public String getImei_1() {
+			return imei_1;
+		}
+		public void setImei_1(String imei_1) {
+			this.imei_1 = imei_1;
+		}
+		public String getImei_2() {
+			return imei_2;
+		}
+		public void setImei_2(String imei_2) {
+			this.imei_2 = imei_2;
+		}
+		public Integer getSimId_1() {
+			return simId_1;
+		}
+		public void setSimId_1(Integer simId_1) {
+			this.simId_1 = simId_1;
+		}
+		public Integer getSimId_2() {
+			return simId_2;
+		}
+		public void setSimId_2(Integer simId_2) {
+			this.simId_2 = simId_2;
+		}
+	}
+	
+	private static DoubleCardInfo initMtkDoubleSim(Context mContext) {
+		DoubleCardInfo mtkDoubleInfo = new DoubleCardInfo();
+		try {
+			TelephonyManager tm = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+			Class<?> c = Class.forName("com.android.internal.telephony.Phone");
+			Field fields1 = c.getField("GEMINI_SIM_1");
+			fields1.setAccessible(true);
+			mtkDoubleInfo.setSimId_1((Integer) fields1.get(null));
+			Field fields2 = c.getField("GEMINI_SIM_2");
+			fields2.setAccessible(true);
+			mtkDoubleInfo.setSimId_2((Integer) fields2.get(null));
+			Method m = TelephonyManager.class.getDeclaredMethod("getSubscriberIdGemini", int.class);
+			mtkDoubleInfo.setImsi_1((String) m.invoke(tm, mtkDoubleInfo.getSimId_1()));
+			mtkDoubleInfo.setImsi_2((String) m.invoke(tm, mtkDoubleInfo.getSimId_2()));
+
+			Method m1 = TelephonyManager.class.getDeclaredMethod("getDeviceIdGemini", int.class);
+			mtkDoubleInfo.setImei_1((String) m1.invoke(tm, mtkDoubleInfo.getSimId_1()));
+			mtkDoubleInfo.setImei_2((String) m1.invoke(tm, mtkDoubleInfo.getSimId_2()));
+
+			Method mx = TelephonyManager.class.getDeclaredMethod("getPhoneTypeGemini", int.class);
+			mtkDoubleInfo.setPhoneType_1((Integer) mx.invoke(tm, mtkDoubleInfo.getSimId_1()));
+			mtkDoubleInfo.setPhoneType_2((Integer) mx.invoke(tm, mtkDoubleInfo.getSimId_2()));
+
+			if (TextUtils.isEmpty(mtkDoubleInfo.getImsi_1()) && (!TextUtils.isEmpty(mtkDoubleInfo.getImsi_2()))) {
+				mtkDoubleInfo.setDefaultImsi(mtkDoubleInfo.getImsi_2());
+			}
+			if (TextUtils.isEmpty(mtkDoubleInfo.getImsi_2()) && (!TextUtils.isEmpty(mtkDoubleInfo.getImsi_1()))) {
+				mtkDoubleInfo.setDefaultImsi(mtkDoubleInfo.getImsi_1());
+			}
+		} catch (Exception e) {
+			mtkDoubleInfo.setMtkDoubleSim(false);
+			return mtkDoubleInfo;
+		}
+		mtkDoubleInfo.setMtkDoubleSim(true);
+		return mtkDoubleInfo;
+	}
+	
+	private static DoubleCardInfo initQualcommDoubleSim(Context mContext) {
+		DoubleCardInfo gaotongDoubleInfo = new DoubleCardInfo();
+		gaotongDoubleInfo.setSimId_1(0);
+		gaotongDoubleInfo.setSimId_2(1);
+		try {
+			Class<?> cx = Class.forName("android.telephony.MSimTelephonyManager");
+			Object obj = mContext.getSystemService("phone_msim");
+
+			Method md = cx.getMethod("getDeviceId", int.class);
+			Method ms = cx.getMethod("getSubscriberId", int.class);
+
+			gaotongDoubleInfo.setImei_1((String) md.invoke(obj, gaotongDoubleInfo.getSimId_1()));
+			gaotongDoubleInfo.setImei_2((String) md.invoke(obj, gaotongDoubleInfo.getSimId_2()));
+			gaotongDoubleInfo.setImsi_1((String) ms.invoke(obj, gaotongDoubleInfo.getSimId_1()));
+			gaotongDoubleInfo.setImsi_2((String) ms.invoke(obj, gaotongDoubleInfo.getSimId_2()));
+		} catch (Exception e) {
+			gaotongDoubleInfo.setGaotongDoubleSim(false);
+			return gaotongDoubleInfo;
+		}
+		gaotongDoubleInfo.setGaotongDoubleSim(true);
+		return gaotongDoubleInfo;
 	}
 
 	/** 获取SDK号 */
